1bbe36fbff00c598a072b7c4445d3615862aa712
Ahmad Meghani <therealmeghani@gmail.com>
2019-02-27 15:52:24 +0500

    Instagram Clone - Activity Navigation for BottomNavigationView

--- app/src/main/java/com/example/instagramclone/ProfileActivity.java
+++ app/src/main/java/com/example/instagramclone/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.instagramclone;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -10,18 +11,17 @@ import android.view.MenuItem;
 import com.example.instagramclone.utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
-    private static final int ACTIVITY_NUM = 0;
-    private Context nContext = MainActivity.this;
+public class ProfileActivity extends AppCompatActivity {
+    private static final String TAG = "ProfileActivity";
+    private static final int ACTIVITY_NUM = 4;
+    private Context nContext = ProfileActivity.this;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Starting");
+        Log.d(TAG, "onCreate: Working!!");
+
         setupBottomNavigationView();
     }
 
--- app/src/main/java/com/example/instagramclone/SearchActivity.java
+++ app/src/main/java/com/example/instagramclone/SearchActivity.java
@@ -2,6 +2,7 @@ package com.example.instagramclone;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -10,18 +11,17 @@ import android.view.MenuItem;
 import com.example.instagramclone.utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "MainActivity";
-
-    private static final int ACTIVITY_NUM = 0;
-    private Context nContext = MainActivity.this;
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+    private static final int ACTIVITY_NUM = 1;
+    private Context nContext = SearchActivity.this;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: Starting");
+        Log.d(TAG, "onCreate: Working!!");
+
         setupBottomNavigationView();
     }
 
@@ -37,5 +37,6 @@ public class MainActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+
     }
 }
