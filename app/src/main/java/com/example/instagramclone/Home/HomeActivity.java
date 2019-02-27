2cb56e538e37b5a28017ee238978c8ff4b58fa69
Ahmad Meghani <therealmeghani@gmail.com>
2019-02-27 20:55:46 +0500

    Instagram Clone - SectionsPagerAdapter Tabs

--- app/src/main/java/com/example/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/instagramclone/Home/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.instagramclone.Home;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -24,6 +26,28 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Starting");
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * responsible for adding the 3 tabs: camera, home and messages
+     */
+
+    public void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragments(new CameraFragment());
+        adapter.addFragments(new HomeFragment());
+        adapter.addFragments(new DMsFragment());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_instagram);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
     }
 
     /**
